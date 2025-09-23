2fa65e981464a414feac8d29374aac01d384c234
getto.fahjar <emailanda@contoh.comgetto.udonindonesia@gmail.com>
2025-09-23 22:55:43 +0700

    Mengubah puzzle menjadi mekanisme tukar dan memperbaiki bug ubin kosong

--- js/puzzle.js
+++ js/puzzle.js
@@ -5,7 +5,7 @@ class PuzzleGame {
         this.imagePath = imagePath;
         this.board = [];
         this.tiles = [];
-        this.emptyTileIndex = 0;
+        this.emptyTileIndex = numTiles - 1;
         this.moves = 0;
         this.timer = 0;
         this.intervalId = null;
@@ -64,58 +64,31 @@ class PuzzleGame {
         tile.classList.add('puzzle-tile');
         tile.dataset.value = tileValue;
 
-        if (tileValue === this.emptyTileIndex) {
-            tile.classList.add('empty-tile');
-            tile.textContent = '';
-        } else {
-            const row = Math.floor(tileValue / this.gridSize);
-            const col = tileValue % this.gridSize;
-            tile.style.backgroundImage = `url('${this.imagePath}')`;
-            tile.style.backgroundSize = `${this.gridSize * 100}% ${this.gridSize * 100}%`;
-            tile.style.backgroundPosition = `-${col * 100}% -${row * 100}%`;
-        }
         this.puzzleBoardElement.appendChild(tile);
     });
     this.board = Array.from(this.puzzleBoardElement.children);
 }
 
     shuffleTiles() {
-        let shuffled = [...this.tiles];
-        const numTiles = this.gridSize * this.gridSize;
-        do {
-            for (let i = shuffled.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-            }
-        } while (!this.isSolvable(shuffled));
-        this.tiles = shuffled;
+    let shuffled = [...this.tiles];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
+    this.tiles = shuffled;
+}
 
-    isSolvable(currentTiles) {
-        const flatTiles = currentTiles.filter(val => val !== this.emptyTileIndex);
-        let inversions = 0;
-        for (let i = 0; i < flatTiles.length - 1; i++) {
-            for (let j = i + 1; j < flatTiles.length; j++) {
-                if (flatTiles[i] > flatTiles[j]) {
-                    inversions++;
-                }
-            }
-        }
-
-        const emptyRow = Math.floor(currentTiles.indexOf(this.emptyTileIndex) / this.gridSize);
-        if (this.gridSize % 2 === 1) { // Odd grid (3x3, 5x5)
-            return inversions % 2 === 0;
-        } else { // Even grid (2x2, 4x4, 6x6)
-            // From bottom, 1-indexed
-            const emptyRowFromBottom = this.gridSize - emptyRow;
-            if (emptyRowFromBottom % 2 === 1) { // Empty tile on odd row from bottom
-                return inversions % 2 === 0;
-            } else { // Empty tile on even row from bottom
-                return inversions % 2 === 1;
+  isSolvable() {
+    let inversions = 0;
+    for (let i = 0; i < this.tiles.length - 1; i++) {
+        for (let j = i + 1; j < this.tiles.length; j++) {
+            if (this.tiles[i] > this.tiles[j]) {
+                inversions++;
             }
         }
     }
-
+    return inversions % 2 === 0;
+}
 
     addEventListeners() {
         this.puzzleBoardElement.addEventListener('click', this.handleTileClick.bind(this));
@@ -133,75 +106,56 @@ class PuzzleGame {
             this.isGameActive = true; // Pastikan game aktif saat klik pertama
         }
 
-        const clickedValue = parseInt(clickedTile.dataset.value);
-        const clickedIndex = this.tiles.indexOf(clickedValue);
+          const clickedIndex = this.board.indexOf(clickedTile);
+          clickedTile.classList.add('selected'); // Tandai potongan yang diklik
 
-        if (this.canMove(clickedIndex)) {
-            this.moveTile(clickedIndex);
-            this.moves++;
-            this.updateUI();
+          if (!this.firstClick) {
+        this.firstClick = { tile: clickedTile, index: clickedIndex };
+    } else {
+        this.secondClick = { tile: clickedTile, index: clickedIndex };
+        this.swapTiles(this.firstClick.index, this.secondClick.index);
 
-            clickedTile.classList.add('clicked');
-            setTimeout(() => {
-                clickedTile.classList.remove('clicked');
-            }, 200); // Durasi animasi CSS
+        // Hapus seleksi
+        this.firstClick.tile.classList.remove('selected');
+        this.secondClick.tile.classList.remove('selected');
+        this.firstClick = null;
+        this.secondClick = null;
 
-            if (this.checkWin()) {
-                this.stopTimer();
-                this.isGameActive = false;
-                // Panggil showPopup dari ui.js
-                showPopup(`Selamat! Anda berhasil menyelesaikan puzzle dalam ${formatTime(this.timer)} dengan ${this.moves} gerakan!`, this.level, this.moves, this.timer);
-            }
+        this.moves++;
+        this.updateUI();
+
+        if (this.checkWin()) {
+            this.stopTimer();
+            this.isGameActive = false;
+            showPopup(`Selamat! Anda berhasil menyelesaikan puzzle dalam ${formatTime(this.timer)} dengan ${this.moves} gerakan!`, this.level, this.moves, this.timer);
         }
     }
+}
 
-    canMove(clickedIndex) {
-        const [clickedRow, clickedCol] = [Math.floor(clickedIndex / this.gridSize), clickedIndex % this.gridSize];
-        const [emptyRow, emptyCol] = [Math.floor(this.tiles.indexOf(this.emptyTileIndex) / this.gridSize), this.tiles.indexOf(this.emptyTileIndex) % this.gridSize];
+        // Fungsi baru untuk menukar ubin secara visual
+    swapTiles(index1, index2) {
+    const tile1 = this.board[index1];
+    const tile2 = this.board[index2];
 
-        const isAdjacentRow = Math.abs(clickedRow - emptyRow) === 1 && clickedCol === emptyCol;
-        const isAdjacentCol = Math.abs(clickedCol - emptyCol) === 1 && clickedRow === emptyRow;
+    // Tukar posisi di array internal
+    [this.tiles[index1], this.tiles[index2]] = [this.tiles[index2], this.tiles[index1]];
 
-        return isAdjacentRow || isAdjacentCol;
-    }
+    // Tukar properti DOM
+    const tempInnerHTML = tile1.innerHTML;
+    const tempClasses = tile1.className;
+    const tempStyle = tile1.style.cssText;
+    const tempDatasetValue = tile1.dataset.value;
 
-    moveTile(clickedIndex) {
-        const emptyIndex = this.tiles.indexOf(this.emptyTileIndex);
+    tile1.innerHTML = tile2.innerHTML;
+    tile1.className = tile2.className;
+    tile1.style.cssText = tile2.style.cssText;
+    tile1.dataset.value = tile2.dataset.value;
 
-        // Swap values in the internal tiles array
-        [this.tiles[clickedIndex], this.tiles[emptyIndex]] = [this.tiles[emptyIndex], this.tiles[clickedIndex]];
-
-        // Update DOM elements (visually swap their content/styles)
-        const clickedElement = this.board[clickedIndex];
-        const emptyElement = this.board[emptyIndex];
-
-        // Temporarily store properties of the clicked element
-        const tempInnerHTML = clickedElement.innerHTML;
-        const tempClasses = clickedElement.className; // Use className to get all classes
-        const tempStyle = clickedElement.style.cssText;
-        const tempDatasetValue = clickedElement.dataset.value;
-
-
-        // Copy properties from emptyElement to clickedElement's new position
-        clickedElement.innerHTML = emptyElement.innerHTML;
-        clickedElement.className = emptyElement.className;
-        clickedElement.style.cssText = emptyElement.style.cssText;
-        clickedElement.dataset.value = emptyElement.dataset.value; // Now clickedElement is empty
-
-
-        // Copy stored properties from original clickedElement to emptyElement's new position
-        emptyElement.innerHTML = tempInnerHTML;
-        emptyElement.className = tempClasses;
-        emptyElement.style.cssText = tempStyle;
-        emptyElement.dataset.value = tempDatasetValue; // Now emptyElement has content
-
-
-        // Visually swap classes for empty tile
-        clickedElement.classList.remove('empty-tile');
-        emptyElement.classList.add('empty-tile');
-        emptyElement.textContent = ''; // Pastikan kosong
-    }
-
+    tile2.innerHTML = tempInnerHTML;
+    tile2.className = tempClasses;
+    tile2.style.cssText = tempStyle;
+    tile2.dataset.value = tempDatasetValue;
+}
 
     checkWin() {
         for (let i = 0; i < this.tiles.length; i++) {
